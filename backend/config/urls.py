0a61d0443d53c56de98475e4a6240c4a01244130
arsuhinars <arsuhinars@gmail.com>
2023-05-24 18:35:55 +0400

    🎨 Improved config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('docs.urls')),
-    path('', include('auth.urls')),
-    path('', include('users.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("docs.urls")),
+    path("", include("auth.urls")),
+    path("", include("users.urls")),
+    path("", include("candidates.urls")),
 ]
