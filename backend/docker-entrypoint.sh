54474b98504bfe68e8f1f778a5a7cfb59e56dbd1
arsuhinars <arsuhinars@gmail.com>
2023-05-24 13:39:52 +0400

    ✨ Added admin user creation

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,6 +4,11 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
+if [ ! -z $ADMIN_CREDENTIALS_FILE -a -f $ADMIN_CREDENTIALS_FILE ]; then
+export $(egrep -v '^#' $ADMIN_CREDENTIALS_FILE | xargs)
+python manage.py createsuperuser --noinput || echo
+fi
+
 if [ $DEBUG ]; then
 python manage.py runserver 0.0.0.0:$BACKEND_PORT $@
 else
