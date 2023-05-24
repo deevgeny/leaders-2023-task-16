4abd5f4285c3ab075fee2fce71e32a24420aebd2
arsuhinars <arsuhinars@gmail.com>
2023-05-24 18:36:33 +0400

    ✨ Added new exceptions

--- backend/core/exceptions.py
+++ backend/core/exceptions.py
@@ -9,7 +9,19 @@ class PermissionDeniedException(PermissionDenied):
     ...
 
 
+class InvalidFormatException(APIException):
+    status_code = 400
+    default_detail = "Invalid input format"
+    default_code = "invalid_format"
+
+
 class AlreadyExistsException(APIException):
     status_code = 409
-    default_detail = 'Entity with given data already exists'
-    default_code = 'already_exists'
+    default_detail = "Entity with given data already exists"
+    default_code = "already_exists"
+
+
+class IntegrityBreachException(APIException):
+    status_code = 409
+    default_detail = "Entity integrity breach error"
+    default_code = "integrity_breach"
