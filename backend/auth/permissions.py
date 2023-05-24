ea96c6a77b6e68021408aab2009e58f068c61ad1
arsuhinars <arsuhinars@gmail.com>
2023-05-24 13:40:49 +0400

    🔥 Removed unused permissions

--- backend/auth/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-from rest_framework.request import Request
-from rest_framework.views import View
-
-
-class IsAnonymous(BasePermission):
-    def has_permission(self, request: Request, view: View):
-        return request.user is None
